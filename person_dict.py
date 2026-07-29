f75b2d0842c30fd71b3518e134c7d17ba4cd5ee2
AnubhavSinghZ <aanusingh927@gmail.com>
2026-07-29 23:56:47 +0530

    fav number of persons

--- person_dict.py
+++ person_dict.py
@@ -1,3 +1,5 @@
+#The details of a person
+
 person={
     "first_name": "XYZ",
     "last_name": "ABC",
@@ -5,3 +7,12 @@ person={
     "city": "ABCXYz"
 }
 print(f"The Person Details: {person}")
+
+#Favorite Number
+fav_num={
+    "Prince":10,
+    "Anubav":7,
+    "Abhishek":18,
+    "Aman":45
+ }
+print(f"The Favorite Number of Person: {fav_num}")
