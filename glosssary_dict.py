4503c6c36a10a88cec5d0e3e4656a0892ace52fc
AnubhavSinghZ <aanusingh927@gmail.com>
2026-07-30 22:55:08 +0530

    add a file

---
+++ glosssary_dict.py
@@ -0,0 +1,8 @@
+glossary={
+    "list": " is a data type",
+    "tuple": ",it is also a data type",
+    "dict": ",it is a key value pair",
+    "condi...": ",if else elif",
+}
+for name, values in glossary.items():
+    print(f"{name + ":"} \n{values}  ")
