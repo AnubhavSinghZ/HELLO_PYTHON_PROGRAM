70803db6966593cb439e9f37dd769e56d3fc5d02
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-08 23:16:40 +0530

    list in dict

---
+++ pzza_dict.py
@@ -0,0 +1,7 @@
+pizza={
+    'crust': "thick",
+    "toppings": ['mushroom', 'extra cheese'],
+    }
+print(f"You ordered a {pizza['crust']}- crust pizza wit the following toppings")
+for topping in pizza['toppings']:
+    print(f'\t{topping}')
