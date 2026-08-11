d3a19d90db3f4c4496ff6ea680ca25831c428999
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-11 21:49:17 +0530

    Add a code for the ticket price

---
+++ movie_tct.py
@@ -0,0 +1,14 @@
+# A movie theater charge different price for different ages
+
+ticket="Different tickets prices"
+ticket+="\n What is your age?"
+
+while True:
+    age=int(input(ticket))
+
+    if age<3:
+        print("Free Ticket")
+    elif age<12:
+        print("The Ticket price is $10")
+    else:
+        print("the ticket price is $15")
