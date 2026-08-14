5680068437756556efc07f75186ef6cd613f3837
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-14 23:54:27 +0530

    add comment

--- sandwiches.py
+++ sandwiches.py
@@ -1,7 +1,7 @@
 sandwiches_orders=['desert', 'raw', 'cold', 'warm']
-finished_sandwiches=[]
+finished_sandwiches=[]  # Empty list
 while sandwiches_orders:
-    orders=sandwiches_orders.pop()
+    orders=sandwiches_orders.pop()  # Will pop the sandwiches_orders and assign it to orders
     print(f"\nI made your {orders} sandwich")
     finished_sandwiches.append(orders)
 
