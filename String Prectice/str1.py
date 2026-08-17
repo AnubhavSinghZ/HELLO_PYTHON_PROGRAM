2c34d6371f84d185c69ed8fbfa4ee9f5eee8ba93
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-17 22:07:01 +0530

    String slicing and indexing

--- String Prectice/str1.py
+++ String Prectice/str1.py
@@ -8,3 +8,15 @@ text="Python"
 #text[0]='J' #<--- Type Error
 #Correct Approach
 text="J"+text[1:]
+
+
+# Indexing  and  Slicing
+
+s="DEVELOPER"
+print(s[0])       # 'D'  (First character)
+print(s[-1])      # 'R'  (Last character)
+print(s[0:4])     # 'DEVE' (Indices 0, 1, 2, 3)
+print(s[2:])      # 'VELOPER'
+print(s[:4])      # 'DEVE'
+print(s[::2])     # 'DVLOE' (Step by 2)
+print(s[::-1])   # 'REPOLEVED' (Reverses string)
