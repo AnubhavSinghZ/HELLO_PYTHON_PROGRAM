29a58cf4461a9256c1c224a37a7bee1f9721c19a
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-18 22:40:09 +0530

    Practice string function

--- String Prectice/str2.py
+++ String Prectice/str2.py
@@ -1,30 +1,45 @@
-#lower() function
+#1 lower() function
 
 text= "HELLO"
 print(text.lower())
 
-#upper() function
+#2 upper() function
 
 abc="hello"
 print(abc.upper())
 
 
-#strip() function
+#3 strip() function
 
 xyz="  Python  "
 print(xyz.strip())
 
-#replace() function
+#4 replace() function
 ok="I like Java"
 print(ok.replace("Java", "Python"))
 
-#pop() function
+#5 pop() function
 
 items=["A", "B", "C"]
 print(items.pop()) # will pop form last index
 print(items)
 
-#sort() function
+#6 sort() function
 nums=[1,8,6,3]
 nums.sort()
 print(nums)
+
+#7 reverse() function
+
+num=[1,2,3]
+num.reverse()
+print(num)
+
+#8 index() function
+letters=['b', 's', 'c']
+print(letters.index('b'))
+
+# 9 count() function
+
+numbers=(1,2,3,4,5,2)
+print(numbers.count(2))
