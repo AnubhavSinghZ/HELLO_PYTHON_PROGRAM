1f93c648d7c3fb103cb0b26d02f379561a2a1649
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-19 21:44:14 +0530

    swapping of the string

--- String Prectice/swap_string.py
+++ String Prectice/swap_string.py
@@ -2,5 +2,5 @@ word="PROGRAMMING"
 print(word[::-1]) # This reverse the word from end of the word to the start of the word
 middle_5=word[3:8]
 reversed_middle=middle_5[::-1]
-print("Middle 5 characters," ,middle_5)
-print("Reversed middle 5,", reversed_middle)
+print("Middle 5 characters," ,middle_5)  #middle 5 word 
+print("Reversed middle 5,", reversed_middle)   # reverse of the 5 middle word
---
+++ swap.py
@@ -0,0 +1,4 @@
+text="Database"
+swapped_text=text[-1]+text[1:-1]+text[0]
+print("Original::", text)
+print("Swap Word::", swapped_text)
