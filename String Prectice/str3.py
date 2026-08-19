12b91b94decd2960dc8e6a6210d0dfa25e370ef5
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-19 21:37:37 +0530

    Add reverse of the string

---
+++ String Prectice/str3.py
@@ -0,0 +1,6 @@
+word="PROGRAMMING"
+print(word[::-1]) # This reverse the word from end of the word to the start of the word
+middle_5=word[3:8]
+reversed_middle=middle_5[::-1]
+print("Middle 5 characters," ,middle_5)
+print("Reversed middle 5,", reversed_middle)
