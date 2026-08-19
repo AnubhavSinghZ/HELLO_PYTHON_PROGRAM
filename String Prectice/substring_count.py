ed13280ad17c71786069cc511877e02a71265ff4
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-19 21:52:02 +0530

    substring count in the main string

---
+++ String Prectice/substring_count.py
@@ -0,0 +1,4 @@
+substr="Py"
+mainstr="I love Python Code"
+exist=substr.lower() in mainstr.lower()
+print("Does 'Py' exist?::", exist)  # This program will return True or False if the word will be found in the searchable block of code
