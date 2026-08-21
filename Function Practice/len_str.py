9a3523fa68519498143f88aa20f182882271bc01
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-21 21:47:57 +0530

    practice def function

---
+++ Function Practice/len_str.py
@@ -0,0 +1,6 @@
+cities=["delhi", "gurgaon", "gida" "noida", "chennai", "mumbai"]
+heros=["ironman", "spyderman", "thor", "cap america"]
+def print_len(list):
+    print(len(cities))
+print_len(cities)
+print_len(heros) 
