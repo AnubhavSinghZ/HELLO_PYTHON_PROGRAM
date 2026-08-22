116c925230fc9e072b0d7d9c3892f4fba26d7bc6
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-22 23:02:22 +0530

    Add function with difficult task

---
+++ Function Practice/arbitrary.py
@@ -0,0 +1,8 @@
+def myFun(*args, **kwargs):
+    print("Non-Keyword Arguments (*args):")
+    for arg in args:
+        print(arg)
+    print("Keyword Argument (**kwargs):")
+    for key, value in kwargs.items():
+        print(f"{key}=={value}")
+myFun('Hey', 'welcome', first='Geeks', mid='for', last='Geeks')
