cd5408d07354b0e5c24d92bc06910f0c53fc2c95
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-22 23:13:33 +0530

    Add comments for better explanation

--- Function Practice/arbitrary.py
+++ Function Practice/arbitrary.py
@@ -6,3 +6,7 @@ def myFun(*args, **kwargs):
     for key, value in kwargs.items():
         print(f"{key}=={value}")
 myFun('Hey', 'welcome', first='Geeks', mid='for', last='Geeks')
+
+#  *args stores extra positional arguments.
+#  **kwargs stores extra keyword arguments.
+# loop prints all positional and keyword values separately
