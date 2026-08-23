e9d92ba3a5ceb15c25ecc1d5ee4c001169de6adf
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-23 23:23:26 +0530

    Add another mehtod of squaring a num

--- Function Practice/square.py
+++ Function Practice/square.py
@@ -1,4 +1,13 @@
+# User Input for squaring a Number
 num = float(input("Enter the Number for squaring it : "))
 def squa(num):
     return num**2
 print(squa(num))
+
+# Predefined by the code
+
+def squa_num(nums):
+    return nums**2
+print(squa_num(2))
+print(squa_num(100))
+print(squa_num(-4))
