b3edce3123335250e219658d420f5e51f0fca6e0
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-23 22:48:56 +0530

    Add function **kwargs

--- Function Practice/number.py
+++ Function Practice/number.py
@@ -13,3 +13,9 @@ def stat(*args):
     for arg in args:
         print(arg)
 stat(1,2,3,4,5)
+
+# Using **kwargs 
+def star(**kwargs):
+    for k , val in kwargs.items():
+        print(f"{k}: {val}")
+star(name="Olivia", age="30", city="New York")
