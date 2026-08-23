166d44a51a45e4024f9b8683b9e19bc45797cb3d
AnubhavSinghZ <aanusingh927@gmail.com>
2026-08-23 22:43:46 +0530

    function *args

--- Function Practice/square.py
+++ Function Practice/square.py
@@ -1,6 +1,15 @@
+#Passing function as Arguments
+
 def fun(func,arg):
     return func(arg)
 def square(x):
     return x**2
 res = fun(square,5)
 print(res)
+
+#Using *args
+
+def stat(*args):
+    for arg in args:
+        print(arg)
+stat(1,2,3,4,5)
