1271ccb1ffa65c927bebe86beca705fa9240e116
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-09-03 22:26:15 +0530

    changing color

--- alien_color.py
+++ alien_color.py
@@ -9,7 +9,7 @@ else:
     print("YOU HAVE EARNED 10 POINTS")
 if (alien_color=="green") or (alien_color=="GREEN") or (alien_color=="Green"):
     print("YOU HAVE EARNED 5 POINTS")
-elif(alien_color=="yellow") or (alien_color=="YELLOW") :
+elif(alien_color=="yellow") or (alien_color=="YELLOW") or (alien_color=="Yellow"):
     print("YOU HAVE EARNED 15 POINTS")
 elif(alien_color=='red') or (alien_color=="RED") or (alien_color=="Red"):
     print("YOU HAVE EARNED 20 POINTS")
