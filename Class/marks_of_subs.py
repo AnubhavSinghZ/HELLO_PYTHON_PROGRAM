bcace4a29134bdaf8f8bd582fc62c0d135c7f4ad
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-06 22:32:24 +0530

    3 students marks and average

---
+++ Class/marks_of_subs.py
@@ -0,0 +1,7 @@
+class Studnet:
+    def __init__(self,name, marks):
+        self.name = name
+        self.marks=marks
+s1= Studnet("Karan", [99,99,99])
+print(s1.name,s1.marks) 
+print(s1.marks/3)
