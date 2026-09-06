ed88fa629fd81607d0774fce3ab73b419037b61b
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-06 22:36:13 +0530

    New code

--- Class/marks_of_subs.py
+++ Class/marks_of_subs.py
@@ -2,6 +2,11 @@ class Studnet:
     def __init__(self,name, marks):
         self.name = name
         self.marks=marks
+    def get_avg(self):
+        sum=0
+        for val in self.marks:
+            sum+=val
+        print("Hi", self.name,"Your average marks is:", sum/3)
+
 s1= Studnet("Karan", [99,99,99])
 print(s1.name,s1.marks) 
-print(s1.marks/3)
