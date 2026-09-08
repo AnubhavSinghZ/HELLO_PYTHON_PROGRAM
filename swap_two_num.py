5cb9ecc629a907bd6271f710fd3af1e5f1752a05
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-08 20:25:58 +0530

    3rd method is using bitwise xor

--- swap_two_num.py
+++ swap_two_num.py
@@ -1,8 +1,6 @@
+#Method 1 : Using Temporary Variable
 num1=int(input("Enter the value of number 1="))
 num2=int(input("Enter the value of number 2="))
-
-#Method 1 : Using Temporary Variable
-
 temp =num1
 num1=num2
 num2=temp
@@ -14,4 +12,12 @@ number2=int(input("enter the number2:"))
 number1= number1+number2
 number2=number1-number2
 number1=number1-number2
-print(f"The numbers are {num1} & {num2}")
+print(f"The numbers are {number1} & {number2}")
+
+#Method 3 : Bitwise XOR
+mark1=int(input("enter the mark 1 for swap:"))
+mark2=int(input("enter the mark 2 for swap:"))
+mark1=mark1 ^ mark2
+mark2=mark1 ^ mark2
+mark1=mark1 ^ mark2
+print(f"Mark after swapping mark1={mark1},mark2={mark2}")
