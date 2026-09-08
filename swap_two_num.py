e6530b1dd70d5715a62b6f34d082e17c50ccf786
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-08 20:07:52 +0530

    method 1 using temporary variable

--- swap_two_num.py
+++ swap_two_num.py
@@ -1,2 +1,9 @@
-num1=float(input("Enter the value of number 1="))
-num2=float(input("Enter the value of number 2="))
+num1=int(input("Enter the value of number 1="))
+num2=int(input("Enter the value of number 2="))
+
+#Method 1 : Using Temporary Variable
+
+temp =num1
+num1=num2
+num2=temp
+print(f"The value of num1 and num2 after swapping is {num1}, {num2} respectively")
