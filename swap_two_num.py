1e7f2c69066433b3f58212373f1a41aa7899e2d4
AnubhavSinghZ <aanusingh927@gmail.com>
2026-09-08 20:18:17 +0530

    adding second method

--- swap_two_num.py
+++ swap_two_num.py
@@ -7,3 +7,11 @@ temp =num1
 num1=num2
 num2=temp
 print(f"The value of num1 and num2 after swapping is {num1}, {num2} respectively")
+
+#Method 2 : Arithmetic Operators (Addition and Substraction)
+number1=int(input("Enter the number1:"))
+number2=int(input("enter the number2:"))
+number1= number1+number2
+number2=number1-number2
+number1=number1-number2
+print(f"The numbers are {num1} & {num2}")
